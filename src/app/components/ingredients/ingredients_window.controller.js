33e5cbf29168755a808952f2d83a8a472162263f
Leosten <leonard.leungchongwo@gmail.com>
2016-02-20 23:40:38 +0100

    correction

--- src/app/components/ingredients/ingredients_window.controller.js
+++ src/app/components/ingredients/ingredients_window.controller.js
@@ -1,10 +1,11 @@
-angular.module('marmiton').controller('ingredientsWindowController', ['$scope', "$http", function($scope, $http) {
+angular.module('marmiton').controller('ingredientsWindowController', ['$scope', '$http', function($scope, $http) {
 
 	ingredients_url = "http://127.0.0.1/api/listrecipes/getIngredients.php?recipeid=".concat($scope.recipeid);
 
 	$http.get(ingredients_url).then(function (response){
-	        $scope.ingredients_obj = response.data;
+	        $scope.ingredients = response.data;
+	        console.log($scope.ingredients);
 	    }, function() {
-	        console.log("unable to obtain API data: ingredients! ;)");
+	        console.log("unable to obtain API data: ingredients_window! ;)");
 	    });
 }]);
--- src/app/controllers/recipepage.controller.js
+++ src/app/controllers/recipepage.controller.js
@@ -3,7 +3,6 @@ angular.module('marmiton').controller('recipePage', ['$scope', '$stateParams', "
 $scope.recipeid = $stateParams.recipeid;
 $scope.recipe_details = {};
 recipe_url = "http://127.0.0.1/api/listrecipes/getFullRecipe.php?recipeid=".concat($scope.recipeid);
-ingredients_url = "http://127.0.0.1/api/listrecipes/getIngredients.php?recipeid=".concat($scope.recipeid);
 
 $http.get(recipe_url).then(function (response){
         $scope.recipe_details = response.data;
