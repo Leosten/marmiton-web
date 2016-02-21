0b80f334a2dfa9fed244dfab3ad1a02ac9ea8f5b
Leosten <leonard.leungchongwo@gmail.com>
2016-02-21 03:02:13 +0100

    Formulaire front

--- src/app/controllers/newrecipe.controller.js
+++ src/app/controllers/newrecipe.controller.js
@@ -1,14 +1,35 @@
-//var app = angular.module('marmiton');
-
 angular.module('marmiton').controller('NewRecipe', ['$scope', 'recipeListFactory', function($scope, recipeListFactory) {
-    	
-		$scope.createrecipe = function() {
-        $scope.spice = 'chili';
-    };
-
     $scope.newrecipe = {};
+    recipe = {}
+    ingredient = {};
+    $scope.ingredients_list = [];
 
+    $scope.submitRecipe = function() {
+      recipe = {
+        nom: $scope.recipe_name,
+        difficulte: $scope.difficulty,
+        categorie: $scope.category,
+        tags: $scope.optionaltags,
+      }
+    }
 
+    $scope.addIngredient = function() {
+        ingredient = {
+        nom: $scope.ingredient_name,
+        quantite: $scope.ingredient_quantity,
+        unite: $scope.ingredient_unit
+      };
+      if (ingredient.nom == undefined || ingredient.quantite == undefined) {
+        $scope.error = "Ingrédients invalides!" 
+      } else {
+        $scope.error = null;
+        $scope.ingredient_name = null;
+        $scope.ingredient_quantity = null;
+        $scope.ingredient_unit = null;
+        $scope.ingredients_list = $scope.ingredients_list.concat(ingredient);
+        console.log($scope.ingredients_list);
+      }
+    };
     
 }]);
 
---
+++ src/app/controllers/search.controller.js
@@ -0,0 +1,3 @@
+angular.module('marmiton').controller('searchController', ['$scope', '$stateParams', "$http", function($scope, $stateParams, $http) {
+
+}]);
